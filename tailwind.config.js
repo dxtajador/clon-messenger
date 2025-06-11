bc34a66f2429f7be03524d5a0b47fcf7a1820a3c
joshoa trejo <joshoat100@gmail.com>
2025-06-11 12:16:42 -0700

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        messenger: {
+          light: '#e5d4ff',
+          base: '#8b5cf6',
+          dark: '#6b21a8',
+          background: '#f3f0ff',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
